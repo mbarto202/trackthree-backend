76978b01ecf7bc055264e2b7e1cdb077747e7069
Michael <michael.d.barto@gmail.com>
2025-10-02 20:43:24 -0400

    spring initializr

---
+++ src/test/java/com/trackthree/tracker/TrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trackthree.tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
