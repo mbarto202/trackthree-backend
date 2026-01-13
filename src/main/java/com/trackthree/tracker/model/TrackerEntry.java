1d63fff35cb369f592921700beb64eabb874ded1
Michael <michael.d.barto@gmail.com>
2026-01-12 22:49:07 -0500

    updated entity for clientcode

--- src/main/java/com/trackthree/tracker/model/TrackerEntry.java
+++ src/main/java/com/trackthree/tracker/model/TrackerEntry.java
@@ -4,59 +4,50 @@ import jakarta.persistence.*;
 import java.time.LocalDate;
 
 @Entity
+@Table(
+    name = "tracker_entry",
+    uniqueConstraints = @UniqueConstraint(columnNames = {"client_code", "date"})
+)
 public class TrackerEntry {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "client_code", nullable = false, length = 64)
+    private String clientCode;
+
+    @Column(nullable = false)
     private LocalDate date;
+
     private int calories;
     private int protein;
     private int water;
 
     public TrackerEntry() {}
 
-    public TrackerEntry(LocalDate date, int calories, int protein, int water) {
+    public TrackerEntry(String clientCode, LocalDate date, int calories, int protein, int water) {
+        this.clientCode = clientCode;
         this.date = date;
         this.calories = calories;
         this.protein = protein;
         this.water = water;
     }
 
-    public Long getId() {
-        return id;
-    }
+     public Long getId() { return id; }
 
-    public LocalDate getDate() {
-        return date;
-    }
+    public String getClientCode() { return clientCode; }
+    public void setClientCode(String clientCode) { this.clientCode = clientCode; }
 
-    public void setDate(LocalDate date) {
-        this.date = date;
-    }
+    public LocalDate getDate() { return date; }
+    public void setDate(LocalDate date) { this.date = date; }
 
-    public int getCalories() {
-        return calories;
-    }
+    public int getCalories() { return calories; }
+    public void setCalories(int calories) { this.calories = calories; }
 
-    public void setCalories(int calories) {
-        this.calories = calories;
-    }
+    public int getProtein() { return protein; }
+    public void setProtein(int protein) { this.protein = protein; }
 
-    public int getProtein() {
-        return protein;
-    }
-
-    public void setProtein(int protein) {
-        this.protein = protein;
-    }
-
-    public int getWater() {
-        return water;
-    }
-
-    public void setWater(int water) {
-        this.water = water;
-    }
+    public int getWater() { return water; }
+    public void setWater(int water) { this.water = water; }
 }
