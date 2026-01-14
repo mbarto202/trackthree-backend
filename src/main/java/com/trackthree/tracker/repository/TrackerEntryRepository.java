faa60f105dea9fb2c2621f5e10e4232ce6f89083
Michael <michael.d.barto@gmail.com>
2026-01-13 22:46:04 -0500

    updated entryrepo to find by client code

--- src/main/java/com/trackthree/tracker/repository/TrackerEntryRepository.java
+++ src/main/java/com/trackthree/tracker/repository/TrackerEntryRepository.java
@@ -1,8 +1,13 @@
 package com.trackthree.tracker.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import com.trackthree.tracker.model.TrackerEntry;
 
 public interface TrackerEntryRepository extends JpaRepository<TrackerEntry, Long> {
+    Optional<TrackerEntry> findByClientCodeAndDate(String clientCode, LocalDate date);
+    List<TrackerEntry> findAllByClientCodeOrderByDateDesc(String clientCode);
 }
