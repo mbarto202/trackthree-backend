39d9763aba747d813213baffa074b5aaf3dd963a
Michael <michael.d.barto@gmail.com>
2026-09-01 23:17:10 -0400

    feat: add client listing repository query

--- src/main/java/com/trackthree/tracker/repository/ClientRepository.java
+++ src/main/java/com/trackthree/tracker/repository/ClientRepository.java
@@ -2,6 +2,8 @@ package com.trackthree.tracker.repository;
 
 import com.trackthree.tracker.model.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface ClientRepository extends JpaRepository<Client, String> {
+    List<Client> findAllByAdminFalseOrderByCodeAsc();
 }
